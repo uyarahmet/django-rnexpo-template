cb8094ec3bf6dcca65d30e4d3504b3870eaf0706
Ahmet Uyar <auyar19@ku.edu.tr>
2022-01-23 20:33:39 +0300

    added road

--- App.js
+++ App.js
@@ -1,16 +1,27 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image, Dimensions } from 'react-native';
+
+const win = Dimensions.get('window');
+const ratio = win.width/541; //541
+
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+
+      <Image source={require("./assets/pngegg.png")} style={styles.road}/>
       <StatusBar style="auto" />
     </View>
   );
 }
 
 const styles = StyleSheet.create({
+  road: {
+    flex: 1,
+    alignSelf: 'stretch',
+    width: win.width,
+    height: win.height,
+  },
   container: {
     flex: 1,
     backgroundColor: '#fff',
