c8bf363c6a6a06d588a860bc637c4ecaea39c77f
Ahmet Uyar <auyar19@ku.edu.tr>
2022-02-01 16:39:05 +0300

    added react navigations for login, signup, and home page.

---
+++ components/styles.js
@@ -0,0 +1,11 @@
+import styled from 'styled-components';
+import {View} from 'react-native'
+
+// colors
+const Colors = {
+  primary: '#6305dc',
+  secondary: '#b39bdB',
+  tertiary: '#1f2937'
+}
+
+const {primary, secondary, tertiary} = Colors;
